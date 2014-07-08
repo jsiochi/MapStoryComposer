df7a69a4b3a3ea741969267f48bff69773871636
jsiochi <jeremiahsiochi@gmail.com>
2014-07-08 11:40:40 -0400

    slides now in carousel

--- js/controllers.js
+++ js/controllers.js
@@ -6,4 +6,9 @@ angular.module('storylayers.controllers', ['storylayers.services'])
         
         var yes = dataLoader.load('https://dl.dropboxusercontent.com/u/63253018/TestData.json');
         
+        $scope.slides = [{image: '../MapStoryComposer/img/styleslides/PTsimple.png', active: true},
+                         {image: '../MapStoryComposer/img/styleslides/LNchoropleth.png', active: false}, 
+                         {image: '../MapStoryComposer/img/styleslides/LNsimple.png', active: false},
+                         {image: '../MapStoryComposer/img/styleslides/LNsymbols.png', active: false},
+                         {image: '../MapStoryComposer/img/styleslides/LNunique.png', active: false}];
     }]);
